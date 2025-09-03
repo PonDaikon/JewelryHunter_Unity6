fb44c7a3a601e586815a79ae893d4878f0419699
DESKTOP-NPP6C90\ronom <r.onomitsu@gmail.com>
2025-09-03 12:04:19 +0900

    地面定義

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,14 @@ public class PlayerController : MonoBehaviour
     float axisH; //入力の方向を記憶するための変数
     public float speed = 3.0f; //プレイヤーのスピードを調整
 
+    public float jumpPower = 9.0f;
+    bool goJump = false; //ジャンプフラグ（true:真on false:偽off）
+
+    bool onGround = false; //地面にいるかどうかの判定（地面にいる:true 地面にいない:false）
+
+    public LayerMask groundLayer; //地面レイヤーを指名するための変数
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -19,15 +27,56 @@ public class PlayerController : MonoBehaviour
         //Velocityの元となる値の取得（右なら1.0f、左なら-1.0f、なにもなければ0)
         axisH = Input.GetAxisRaw("Horizontal");
 
+        if (axisH > 0)
+        {
+            //右を向く
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (axisH < 0) 
+            {
+            transform.localScale = new Vector3(-1, 1, 1);
+
+            }
+        //GetButtonDownメソッド→引数に指定したボタンが押されたらtrueを返す、押されていなければfalseを返す
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
 
 
     }
 
+
+
     //1秒間に50回(50fps)繰り返すように制御しながら行う繰り返しメソッド
-    private void FixedUpdate()
+    void FixedUpdate()
     {
+        //地面判定をサークルキャストで行って、その結果を変数onGroundに代入
+        onGround = Physics2D.CircleCast(
+            transform.position, //発射位置＝プレイヤーの位置（基準点）
+            0.2f, //調査する円の半径
+            new Vector2(0,1.0f), //発射方向
+            0, //発射距離
+            groundLayer //対象となるレイヤー情報
+
+            );
+
         //Velocityに値を代入
         rbody.linearVelocity = new Vector2(axisH * speed, rbody.linearVelocity.y);
 
+        if (goJump == true)
+        { 
+            //ジャンプさせる→プレイヤーを上に押し出す
+            rbody.AddForce(new Vector2 (0, jumpPower), ForceMode2D.Impulse);
+            goJump = false; //フラグをOFFに戻す
+        }
+
+
+    }
+
+    //ジャンプボタンが押されたときに呼び出されるメソッド
+    void Jump()
+    { 
+    goJump= true; //ジャンプフラグをon  
     }
 }
